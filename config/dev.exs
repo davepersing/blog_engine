726ce99734da3672c74ac83f3bf0a67591f2bf29
Dave Persing <dave@nebarisoftware.com>
2016-03-28 12:53:40 -0700

    Add posts resources.

--- config/dev.exs
+++ config/dev.exs
@@ -36,7 +36,7 @@ config :phoenix, :stacktrace_depth, 20
 config :blog_engine, BlogEngine.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "blog_engine_dev",
   hostname: "localhost",
   pool_size: 10
